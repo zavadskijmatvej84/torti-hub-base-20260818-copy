37d05208379066aee52da8b9d93451e7afb8f5ed
Torti <zavadskijmatvej84@gmail.com>
2026-08-22 00:02:33 +0300

    Add v18 copy with loader gate

---
+++ access-v18.lua
@@ -0,0 +1,15 @@
+return {
+	version = "v18",
+	enabled = true,
+	killSwitch = false,
+	requireLoader = true,
+	mainFile = "main-v18.lua",
+	loaderFile = "loader-v18.lua",
+	guard = "v18",
+	sessionTtlSeconds = 20,
+	allowedUsernames = {
+		["sergey"] = true,
+	},
+	allowedUserIds = {},
+	allowedPlaceIds = {},
+}
