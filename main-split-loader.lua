1c8638e7efd27850c07c40a34d6a79021eca59cb
Torti <zavadskijmatvej84@gmail.com>
2026-08-21 19:41:46 +0300

    Bump loader cache busters for webhook hotfix delivery

--- main-split-loader.lua
+++ main-split-loader.lua
@@ -1,7 +1,7 @@
 local StarterGui = game:GetService("StarterGui")
 
 local MAIN_URL = "https://raw.githubusercontent.com/zavadskijmatvej84/torti-hub-base-20260818-copy/main/main.lua"
-local BUILD_VERSION = "20260821-live-main-v10"
+local BUILD_VERSION = "20260821-live-main-v11"
 
 local function notify(text)
 	pcall(function()
