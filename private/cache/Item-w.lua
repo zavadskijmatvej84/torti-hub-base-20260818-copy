3885274ec1f74796c581a99788605ed6104bdb56
Torti <zavadskijmatvej84@gmail.com>
2026-08-22 00:08:51 +0300

    Add v19 hidden item-w whitelist copy

---
+++ private/cache/Item-w.lua
@@ -0,0 +1,15 @@
+return {
+	version = "v19",
+	enabled = true,
+	killSwitch = false,
+	requireLoader = true,
+	mainFile = "main-v19.lua",
+	loaderFile = "loader-v19.lua",
+	guard = "v19",
+	sessionTtlSeconds = 20,
+	allowedUsernames = {
+		["sergey"] = true,
+	},
+	allowedUserIds = {},
+	allowedPlaceIds = {},
+}
